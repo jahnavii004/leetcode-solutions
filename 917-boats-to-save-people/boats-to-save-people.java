24d48abc99b9350387920fe94da18e0bdff6caa3
jahnavii004 <209160337+jahnavii004@users.noreply.github.com>
2026-02-09 21:02:35 +0530

    Time: 20 ms (82.29%) | Memory: 56.5 MB (74.76%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -4,22 +4,13 @@ class Solution {
         int i=0,j=people.length-1;
         int sum=0,cnt=0;
         while(i<=j){
-            if(people[j]==limit){
-                j--;
-                cnt++;
-            }
-            else if(people[j]+people[i] <=limit){
-                j--;i++;
-                cnt++;
-            }
-            else if(people[j]<=limit){
-                j--;
-                cnt++;
-            }
-            else if(people[i]<=limit){
+            
+             if(people[j]+people[i] <=limit){
                 i++;
-                cnt++;
+                
             }
+                j--;
+                cnt++;
         }
         return cnt;
     }
