c856c90856ff71f5f46bbbe816bdede5b2281df8
jahnavii004 <209160337+jahnavii004@users.noreply.github.com>
2026-08-18 15:31:23 +0530

    Time: 1 ms (86.46%) | Memory: 44.2 MB (39.52%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -3,15 +3,15 @@ class Solution {
         List<List<Integer>> list= new ArrayList<>();
         int n=nums.length;
         int subset=(int)Math.pow(2,n);
-        for(int num=0;num<subset ;num++){
+        for(int num=0;num<subset ;num++){                   //O(2^n)
             List<Integer> lst=new ArrayList<>();
-            for(int i=0; i<n; i++){
+            for(int i=0; i<n; i++){                     //O(n)
                 if((num & (1 << i)) != 0){
                     lst.add(nums[i]);
                 }
             }
             list.add(lst);
         }
-        return list;
+        return list;                 //sc O(2^n * n)    tc O(2^n * n)
     }
 }
