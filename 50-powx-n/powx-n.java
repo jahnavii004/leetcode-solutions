9df634d26ba09c34952b97103d08d63d9c1254a4
jahnavii004 <209160337+jahnavii004@users.noreply.github.com>
2026-08-21 01:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (93.29%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power= n;
+        if(power <0){
+            power= -1 * power;
+        }
+        double ans=1.0;
+        while(power >0){
+            if( power %2 ==1){
+                ans=ans * x;
+                power =power - 1;
+            }
+            else{
+                x = x*x;
+                power /=2;
+            }
+        }
+        if(n<0){
+            ans = (double)1.0/(double)ans;
+        }
+        return ans;
+    }
+}
